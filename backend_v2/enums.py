0d86204daf46f2560a665a512c61138f12552b25
Marcell Perger <marcell.perger.1@gmail.com>
2025-05-11 12:44:01 +0100

    fix(color-enums): Update the color enums (i.e. reverse da tree)

--- backend_v2/enums.py
+++ backend_v2/enums.py
@@ -5,33 +5,90 @@ from backend_v2.eenum import ExtendableEnum
 __all__ = ['Color', 'PlaceableCardType', 'CardType', 'MoonPhase', 'Area', 'AnyResource']
 
 
-# TODO this must be updated to be compatible with the new ExtendableEnum
-#  (i.e. reverse this 'tree' of enums...)
-class Color(ExtendableEnum):
+class _ColorEnumTree(ExtendableEnum):
+    # Colors
     PURPLE = 1
     GREEN = 2
     RED = 3
     BLUE = 4
     YELLOW = 5
-
-
-class PlaceableCardType(Color):
+    # Card types
     ARTIFACT = 6
-
-
-class CardType(PlaceableCardType):
     EVENT = 7
-
-
-class MoonPhase(Color):
+    # Moon phases
     LAST_TURN = 8
-
-
-class Area(PlaceableCardType):
+    # Locations
     DISCARD = 9
     HAND = 10
-    NONE = 11
-
-
-class AnyResource(Color):
+    SPARE = 11  # Card not used in the current game
+    # Resources
     POINTS = 12
+
+
+class AnyResource(_ColorEnumTree):
+    _eenum_include_members_ = [
+        _ColorEnumTree.PURPLE,
+        _ColorEnumTree.GREEN,
+        _ColorEnumTree.RED,
+        _ColorEnumTree.BLUE,
+        _ColorEnumTree.YELLOW,
+        _ColorEnumTree.POINTS,
+    ]
+
+
+class Area(_ColorEnumTree):
+    _eenum_include_members_ = [
+        _ColorEnumTree.PURPLE,
+        _ColorEnumTree.GREEN,
+        _ColorEnumTree.RED,
+        _ColorEnumTree.BLUE,
+        _ColorEnumTree.YELLOW,
+        _ColorEnumTree.ARTIFACT,
+        _ColorEnumTree.DISCARD,
+        _ColorEnumTree.HAND,
+        _ColorEnumTree.SPARE
+    ]
+
+
+class CardType(_ColorEnumTree):
+    _eenum_include_members_ = [
+        _ColorEnumTree.PURPLE,
+        _ColorEnumTree.GREEN,
+        _ColorEnumTree.RED,
+        _ColorEnumTree.BLUE,
+        _ColorEnumTree.YELLOW,
+        _ColorEnumTree.ARTIFACT,
+        _ColorEnumTree.EVENT
+    ]
+
+
+class PlaceableCardType(Area, CardType):
+    _eenum_include_members_ = [
+        _ColorEnumTree.PURPLE,
+        _ColorEnumTree.GREEN,
+        _ColorEnumTree.RED,
+        _ColorEnumTree.BLUE,
+        _ColorEnumTree.YELLOW,
+        _ColorEnumTree.ARTIFACT,
+    ]
+
+
+class MoonPhase(_ColorEnumTree):
+    _eenum_include_members_ = [
+        _ColorEnumTree.PURPLE,
+        _ColorEnumTree.GREEN,
+        _ColorEnumTree.RED,
+        _ColorEnumTree.BLUE,
+        _ColorEnumTree.YELLOW,
+        _ColorEnumTree.LAST_TURN,
+    ]
+
+
+class Color(AnyResource, PlaceableCardType, MoonPhase):
+    _eenum_include_members_ = [
+        _ColorEnumTree.PURPLE,
+        _ColorEnumTree.GREEN,
+        _ColorEnumTree.RED,
+        _ColorEnumTree.BLUE,
+        _ColorEnumTree.YELLOW,
+    ]
