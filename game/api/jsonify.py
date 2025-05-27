bb51385ad0ebda6bbc467efa555e6d1db9d5390a
Marcell Perger <marcell.perger.1@gmail.com>
2025-05-28 00:28:17 +0100

    feat: Start work on JsonAdapter as a IFrontend implementor

---
+++ game/api/jsonify.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+
+import abc
+from typing import Any, Literal
+
+from ..backend import GameBackend, Player, IFrontend, Card
+
+JsonT = dict[str, Any] | list[Any] | tuple[Any, ...] | float | int | str | bool | None
+
+
+class JsonConnection(abc.ABC):
+    @abc.abstractmethod
+    def send(self, obj: JsonT):
+        ...
+
+    @abc.abstractmethod
+    def receive(self) -> JsonT:
+        ...
+
+
+class JsonAdapter(IFrontend):
+    game: GameBackend
+
+    def __init__(self, conn: JsonConnection):
+        self.conn = conn
+        self._next_thread_id = 1
+
+    def register_game(self, game: GameBackend):
+        self.game = game
+        self.send({'request': 'init',
+                   'server_version': '0.0.1',
+                   'api_version': 1}, thread=False)
+
+    def get_action_type(self, player: Player) -> Literal['buy', 'execute']:
+        # TODO: somehow handle multiple people/clients! - LATER,
+        #  for now, pass-n-play only
+        th = self.send({'request': 'action_type', 'player': player.idx})
+        resp = self.receive(th)
+        # TODO: perhaps repeat if invalid/resend it ?
+        ac_type = resp['action_type']
+        assert ac_type in ('buy', 'execute')
+        return ac_type
+
+    def get_discard(self, player: Player) -> Card:
+        th = self.send({'request': 'discard_for_exec', 'player': player.idx})
+        # TODO: how do we interpret result here - it shouldn't be the entire
+        #  card data - too much can go wrong there, should be some sort of id.
+
+        ...
+
+    def serialise_state(self) -> JsonT:
+        ...  # TODO
+
+    def send(self, obj: dict[str, JsonT], thread=True, state=True):
+        """If thread is True, it returns an opaque 'thread id' that can be
+        used to query replies to this message."""
+        extra = {}
+        if state:
+            extra |= {'state': self.serialise_state()}
+        if (tid := self.alloc_thread() if thread else None) is not None:
+            extra |= {'thread': tid}
+        self.conn.send(obj | extra)
+        return tid
+
+    def receive(self, th: int | None):  # No default so tid isn't accidentally forgotten
+        if th is None:
+            return self.conn.receive()
+        received_th: int = -1  # Can't have this tid, start loop
+        resp = None
+        while received_th != th:
+            # Discard everything else (those referred to older threads,
+            #  can't refer to threads not created yet)
+            resp = self.conn.receive()
+            received_th = resp.pop('thread', -1)
+        return resp
+
+    def alloc_thread(self):
+        th = self._next_thread_id
+        self._next_thread_id += 1
+        return th
+
+    # TODO: these methods need to be implemented:
+    #  - get_spend
+    #  - get_foreach_color
+    #  - choose_from_discard
+    #  - choose_card_exec
+    #  - choose_color_exec_twice
+    #  - choose_excl_color
+    #  - choose_card_move
+    #  - choose_move_where
+    #  - get_card_buy
+    #  - get_discard
