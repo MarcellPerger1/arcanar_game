b04e7b39ee5eff359c714a38780bdcd558aa713e
Marcell Perger <marcell.perger.1@gmail.com>
2025-05-30 23:44:44 +0100

    feat(main): JsonSer is somewhat working now (i.e. no errors)
    Still has many issues. The TODO for the main one is copied below:
    
    TODO: Polymorphism is LOST COMPLETELY here. This is a problem.
    This *must* be fixed for CardEffect to arrive in an intelligible format!
    
    How to fix: detect/code which dataclass has to have type/class info preserved.
    Deser might not need to support this - we might not be sending cards the other way for now.
    Anyway, loading arbitrary classes seems a bit dubious, security-wise.
    
    Also, added a small example in main_v2.py of serialising the entire game state (pre-start, pre-deal cards)

--- main_v2.py
+++ main_v2.py
@@ -1,2 +1,19 @@
-from game.backend import GameBackend
+import json
+import sys
+
+from game.backend import GameBackend, DefaultRuleset
 from game.api.jsonify import *
+
+import pprint
+
+
+class JC(JsonConnection):
+    def send(self, obj: JsonT):
+        print('Sent request:')
+        json.dump(obj, sys.stdout, indent=2)
+
+    def receive(self) -> JsonT:
+        return None
+
+
+GameBackend(4, JsonAdapter(JC()), DefaultRuleset())
