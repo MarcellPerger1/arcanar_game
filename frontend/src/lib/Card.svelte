5cf4c2340a1772ea44a2a7ceb0f761d4951674f2
Marcell Perger <marcell.perger.1@gmail.com>
2025-07-02 18:37:32 +0100

    fix: Finally fix it for small widths

--- frontend/src/lib/Card.svelte
+++ frontend/src/lib/Card.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
   let {data}: {data: CardT} = $props();
+  const LONG = false;
 </script>
 
 <div class={["our-placed-card"].concat(data.is_starting_card ? ["starting-card"] : [])}>
-  {data.is_starting_card ? 'Starting' : 'Placed'}
-  Card
-  Color={data.card_type},
-  Key={data.location.key}
+  {#if LONG}
+    {data.is_starting_card ? 'Starting' : 'Placed'}
+    Card
+    Color={data.card_type},
+    Key={data.location.key}
+  {:else}
+     WWWWW WWW
+     <!-- WWWWW WWW = broken -->
+     <!-- WWWWW WW = works -->
+  {/if}
+  
 </div>
