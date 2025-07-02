af56e741f84fa182b931bcd1666f4302960438eb
Marcell Perger <marcell.perger.1@gmail.com>
2025-07-02 18:38:35 +0100

    fix: Remove temp testing code

--- frontend/src/lib/Card.svelte
+++ frontend/src/lib/Card.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
   let {data}: {data: CardT} = $props();
-  const LONG = false;
 </script>
 
 <div class={["our-placed-card"].concat(data.is_starting_card ? ["starting-card"] : [])}>
-  {#if LONG}
-    {data.is_starting_card ? 'Starting' : 'Placed'}
-    Card
-    Color={data.card_type},
-    Key={data.location.key}
-  {:else}
-     WWWWW WWW
-     <!-- WWWWW WWW = broken -->
-     <!-- WWWWW WW = works -->
-  {/if}
-  
+  {data.is_starting_card ? 'Starting' : 'Placed'}
+  Card
+  Color={data.card_type},
+  Key={data.location.key}
 </div>
