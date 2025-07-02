cc1f8155b764cbc8d8989ec13113903c58cf0941
Marcell Perger <marcell.perger.1@gmail.com>
2025-07-02 20:27:09 +0100

    refactor(frontend): Extract PlayerArea component

--- frontend/src/lib/GamePage.svelte
+++ frontend/src/lib/GamePage.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import MoonPhases from "./MoonPhases.svelte";
-  import Hand from "./Hand.svelte";
-  import OwnPlacedBoard from "./OwnPlacedBoard.svelte";
-  import WithDiscardOnLeft from "./WithDiscardOnLeft.svelte";
-  import { DISCARD, HAND } from "./enums";
+  import PlayerArea from "./PlayerArea.svelte";
 
   let { state }: {state: StateT} = $props();
   let player_data = $derived(state.players[state.curr_player_idx]);
@@ -11,12 +8,5 @@
 
 <div id="game-area">
   <MoonPhases moon_phases={state.moon_phases}/>
-  <div id="our-area-root">
-    <div id="our-area">
-      <Hand cards={Object.values(player_data.areas[HAND])}/>
-      <WithDiscardOnLeft discard_area={player_data.areas[DISCARD]}>
-        <OwnPlacedBoard player_data={player_data}/>
-      </WithDiscardOnLeft>
-    </div>
-  </div>
+  <PlayerArea player_data={player_data}/>
 </div>
