12a2a17a7ae5d5e4f3fa9ddf8b6f63d60c8b1e83
Marcell Perger <marcell.perger.1@gmail.com>
2025-07-05 19:15:39 +0100

    feat: Show resource names properly

--- frontend/src/lib/CardsAndResource.svelte
+++ frontend/src/lib/CardsAndResource.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import PlacedCard from "./PlacedCard.svelte";
-  import { ARTIFACT, type AreaTypeT } from "./enums";
+  import { ARTIFACT, stringifyEnumLong, type AreaTypeT, type ResourceT } from "./enums";
   import type { AreaT } from "./types";
+  import { toCapitalCase } from "./util";
 
-  let {area, areaType, resource}: {area: AreaT, areaType: AreaTypeT, resource: [number, number]} = $props();
+  let {area, areaType, resource}: {area: AreaT, areaType: AreaTypeT, resource: [ResourceT, number]} = $props();
 </script>
 
 <div class={["our-placed-area-column"].concat(areaType == ARTIFACT ? "artifact-column" : "color-area-column")}>
-  <div class="area-column-top-text">R{resource[0]}: {resource[1]}</div>
+  <div class="area-column-top-text">{toCapitalCase(stringifyEnumLong(resource[0]))}: {resource[1]}</div>
   <!-- TODO: extract into CardsColumn?: -->
   <div class="our-placed-card-column">
     {#each Object.values(area) as card_data (card_data.location.key)}
