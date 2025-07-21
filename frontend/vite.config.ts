717a547ab2c1af94934e0e934d270ca516e061ad
Marcell Perger <marcell.perger.1@gmail.com>
2025-07-21 14:10:17 +0100

    ci: Re-enable HMR

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit(), devtoolsJson()],
   server: {
-    // HMR doesn't really work as our websocket backend only handles a single connection
-    // and dies when the connection closes (i.e. after frontend has the inital state)
-    hmr: false,
+    hmr: true,  // Set this to false to remove chance of HMR-related errors
   }
 });
