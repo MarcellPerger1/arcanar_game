50f4000741aa33002a8652549925581e5a3621a1
Marcell Perger <marcell.perger.1@gmail.com>
2025-07-21 10:52:58 +0100

    refactor(frontend): Remove temporary dummy data

--- frontend/src/routes/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { StateT } from "$lib/types";
-import type { PageLoad } from "./$types";
-
-export const load: PageLoad = async ({ fetch }) => {
-  return {
-    state: await (await fetch('dummy_data.json')).json() as StateT
-  };
-}
