42e2a02b4696788e594a4fc1dc1ecadf97b0d679
Marcell Perger <marcell.perger.1@gmail.com>
2025-08-13 19:10:57 +0100

    refactor(frontend): Extract PlacedCardColumn

--- frontend/src/lib/CardsAndResource.svelte
+++ frontend/src/lib/CardsAndResource.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import PlacedCard from "./PlacedCard.svelte";
+  import PlacedCardColumn from './PlacedCardColumn.svelte';
   import { ARTIFACT, stringifyEnumLong, type AreaTypeT, type ResourceT } from "./enums";
   import type { AreaT } from "./types";
   import { toCapitalCase } from "./util";
@@ -9,12 +9,7 @@
 
 <div class={["our-placed-area-column"].concat(areaType == ARTIFACT ? "artifact-column" : "color-area-column")}>
   <div class="area-column-top-text">{toCapitalCase(stringifyEnumLong(resource[0]))}: {resource[1]}</div>
-  <!-- TODO: extract into CardsColumn?: -->
-  <div class="our-placed-card-column">
-    {#each Object.values(area) as card_data (card_data.location.key)}
-       <PlacedCard data={card_data} />
-    {/each}
-  </div>
+  <PlacedCardColumn {area}/>
 </div>
 
 <style>
---
+++ frontend/src/lib/PlacedCardColumn.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import PlacedCard from "./PlacedCard.svelte";
+  import type { AreaT } from "./types";
+
+  let {area}: {area: AreaT} = $props();
+</script>
+
+<div class="our-placed-card-column">
+  {#each Object.values(area) as card_data (card_data.location.key)}
+      <PlacedCard data={card_data} />
+  {/each}
+</div>
+
+<style>
+.our-placed-card-column {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  padding-top: 4px;
+}
+</style>
+
