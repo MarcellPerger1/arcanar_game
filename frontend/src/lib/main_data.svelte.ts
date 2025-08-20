b2da6b4440860219874d4af066d0de87630bc979
Marcell Perger <marcell.perger.1@gmail.com>
2025-08-20 20:38:27 +0100

    feat(frontend+util): Add expectCurrRequest to get specific request types

--- frontend/src/lib/main_data.svelte.ts
+++ frontend/src/lib/main_data.svelte.ts
@@ -1,5 +1,6 @@
 import { getContext, setContext } from "svelte";
-import type { MainStoreT } from "./api";
+import { checkRequestType, type CurrRequestT, type MainStoreT } from "./api";
+import type { ServerReqStrings } from "./types";
 
 const MAIN_DATA_KEY = "MainStore";  // Unique object
 
@@ -13,4 +14,18 @@ export function getData(): MainStoreT {
 /** NOTE: must be wrapped in `$derived(...)` */
 export function getCurrRequest() {
   return getData().currRequest;
+}
+/** NOTE: must be wrapped in `$derived(...)` */
+export function expectCurrRequest<T extends ServerReqStrings>(expect: T): CurrRequestT<T> | never {
+  const result = getCurrRequest();
+  if(!checkRequestType(result, expect)) throw new Error(`Unexpected request type (wanted ${expect}, got ${result?.msg.request})`);
+  return result;
+}
+/** NOTE: must be wrapped in `$derived(...)` */
+export function expectCurrRequestOr<T extends ServerReqStrings>(expect: T): CurrRequestT<T> | undefined;
+export function expectCurrRequestOr<T extends ServerReqStrings, U>(expect: T, other: U): CurrRequestT<T> | U;
+export function expectCurrRequestOr<T extends ServerReqStrings, U>(expect: T, other?: U): CurrRequestT<T> | U | undefined {
+  const result = getCurrRequest();
+  if(!checkRequestType(result, expect)) return other;
+  return result;
 }
