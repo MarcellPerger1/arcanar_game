94516964075260c9b9a1ab8b8e4eb8f87c5d296c
Marcell Perger <marcell.perger.1@gmail.com>
2025-08-20 17:34:58 +0100

    feat(frontend): Add for-now-dyfunctional UI for resource spend

--- frontend/src/lib/PlayerBoardColumn.svelte
+++ frontend/src/lib/PlayerBoardColumn.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
-  import PlacedCardColumn from './PlacedCardColumn.svelte';
-  import { ARTIFACT, stringifyEnumLong, type AreaTypeT, type ResourceT } from "./enums";
-  import type { AreaT } from "./types";
-  import { toCapitalCase } from "./util";
+import PlacedCardColumn from './PlacedCardColumn.svelte';
+import { checkRequestType } from './api/index.ts';
+import { couldBeAcceptableForCost } from './common.ts';
+import { ARTIFACT, checkEnumType, POINTS, Resources, stringifyEnumLong, type AreaTypeT, type ResourceT } from "./enums";
+import { getCurrRequest } from './main_data.svelte.ts';
+import type { AreaT } from "./types";
+import { toCapitalCase } from "./util";
 
-  let {area, areaType, resource}: {area: AreaT, areaType: AreaTypeT, resource: [ResourceT, number]} = $props();
+let {area, areaType, resource: resourcePair}: {area: AreaT, areaType: AreaTypeT, resource: [ResourceT, number]} = $props();
+let [resource, resourceAmount] = $derived(resourcePair);
+let req = $derived(getCurrRequest());
+
+let spendAmount = $state(0);
 </script>
 
-<div class={["our-placed-area-column"].concat(areaType == ARTIFACT ? "artifact-column" : "color-area-column")}>
-  <div class="area-column-top-text">{toCapitalCase(stringifyEnumLong(resource[0]))}: {resource[1]}</div>
+<div class={["our-placed-area-column", areaType == ARTIFACT ? "artifact-column" : "color-area-column"]}>
+  {#if checkRequestType(req, "card_payment")}
+    <div class="center-text board-column-top-text spend-indicator" class:for-spacing-only={!couldBeAcceptableForCost(req.msg.cost, resource)}>
+      <button class="spend-button reset-builtin-appearance">-</button>
+      <div class="spend-amount-text">Spend: {spendAmount}</div>
+      <button class="spend-button reset-builtin-appearance">+</button>
+    </div>
+  {/if}
+  <div class="board-column-top-text center-text">{toCapitalCase(stringifyEnumLong(resource))}: {resourceAmount - spendAmount}</div>
   <PlacedCardColumn {area}/>
 </div>
 
@@ -20,16 +34,29 @@
   align-items: stretch;
 }
 
-.area-column-top-text {
-  text-align: center;
+.board-column-top-text {
+  margin: 3px;
 }
 
-.our-placed-card-column {
-  flex: 1;
+.spend-indicator {
   display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  padding-top: 4px;
+  justify-content: space-between;
+  align-items: baseline;
+}
+.spend-button {
+  margin: 3px;
+  border-radius: 50%;
+  background-color: #888;
+  width: 1.2em;
+  height: 1.2em;
+  font-family: Consolas, 'Courier New', monospace;
+}
+
+.center-text {
+  text-align: center;
+}
+.for-spacing-only {
+  visibility: hidden;
 }
 
 .artifact-column {
