87d226b2461ef23052bc10985ac16c9510335744
Marcell Perger <marcell.perger.1@gmail.com>
2025-08-20 20:37:07 +0100

    fix(backend+util): Fix counter functions not converting from string to desired type

--- frontend/src/lib/common.ts
+++ frontend/src/lib/common.ts
@@ -3,7 +3,7 @@ import type { _Counter, CostT } from "./types";
 
 export function matchesCostExact(cost: CostT, resources: _Counter<ResourceT>) {
   const numProvided = counterTotal(resources);
-  const keys = counterNonzeroKeys(resources);  // What the functional programming
+  const keys = counterNonzeroKeys(resources, Number as (a: `${ResourceT}`) => ResourceT);  // What the functional programming
   return cost.possibilities.some(([filt, n]) => n == numProvided && keys.every(v => filt.allowed_resources.includes(v)));
 }
 export function couldBeAcceptableForCost(cost: CostT, resource: ResourceT) {
@@ -13,6 +13,6 @@ export function couldBeAcceptableForCost(cost: CostT, resource: ResourceT) {
 export function counterTotal(c: _Counter<any>): number {
   return Object.values(c).filter(v => v != null).reduce((a, b) => a + b, 0);
 }
-export function counterNonzeroKeys<T>(c: _Counter<T>): T[] {
-  return (Object.entries(c) as [T, number][]).filter(([_k, v]) => v).map(([k, _v]) => k);
+export function counterNonzeroKeys<T extends string | number | boolean>(c: _Counter<T>, conv: (a: `${T}`) => T): T[] {
+  return (Object.entries(c) as [`${T}`, number][]).filter(([_k, v]) => v).map(([k, _v]) => conv(k));
 }
