29f79e40b4e409053e874edab5616051200f317f
Marcell Perger <marcell.perger.1@gmail.com>
2025-08-24 16:08:45 +0100

    fix(frontend): Ignore no-scopable-elements svelte CSS warning
    This is required because ButtonDiv needs some way of receiveing styles and that way is currently class attribute + `:global`.
    I (obviously) want these styles next to the each usage of ButtonDiv so I refuse to put it in an external CSS file.
    (I'm using ButtonDiv as a custom element rather than a svelte-style component - and it doesn't seem to like that)

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @typedef {import('svelte/compiler').Warning} Warning */
+
+/** @type {import('@sveltejs/vite-plugin-svelte').SvelteConfig['onwarn']} */
+const onwarn = function onwarn(/**@type {Warning}*/warning, /**@type {(Warning) => void}*/defaultHandler) {
+  // I will put my styles next to the relevant HTML (ButtonDiv needs some way of 
+  // receiveing styles and that way is currently class attribute + :global)
+  if(warning.code == 'vite-plugin-svelte-css-no-scopable-elements') return;
+  defaultHandler(warning);
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
@@ -12,7 +22,9 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter()
-  }
+  },
+
+  onwarn
 };
 
 export default config;
