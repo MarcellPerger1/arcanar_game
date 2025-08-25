99657f540a7a01fe3d75f27b78926d7ed150ed46
Marcell Perger <marcell.perger.1@gmail.com>
2025-08-25 11:39:07 +0100

    fix(frontend): Show moon phases as a char rather than the enum number

--- frontend/src/lib/MoonPhases.svelte
+++ frontend/src/lib/MoonPhases.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-  import type { MoonPhasesT } from "./types";
+import { stringifyEnumShort } from "./enums.ts";
+import type { MoonPhasesT } from "./types";
 
-  let { moon_phases }: {moon_phases: MoonPhasesT} = $props();
+let { moon_phases }: {moon_phases: MoonPhasesT} = $props();
 </script>
 
 <div id="moon-phases-area-root">
   <div id="moon-phases-area">
-    {#each {length:5}, i}
+    {#each {length: 6}, i}
       {#if moon_phases[i].length == 2}
-        <div class="single-moon-phase moon-phase-top">{moon_phases[i][0]}</div>
-        <div class="single-moon-phase moon-phase-bottom">{moon_phases[i][1]}</div>
+        <div class="single-moon-phase moon-phase-top">{stringifyEnumShort(moon_phases[i][0])}</div>
+        <div class="single-moon-phase moon-phase-bottom">{stringifyEnumShort(moon_phases[i][1])}</div>
       {:else}
-        <div class="double-moon-phase">{moon_phases[i][0]}</div>
+        <div class="double-moon-phase">{stringifyEnumShort(moon_phases[i][0])}</div>
       {/if}
     {/each}
-    <div class="double-moon-phase">LAST</div>
   </div>
 </div>
 
